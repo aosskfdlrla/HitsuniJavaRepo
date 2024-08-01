cf0295645234a4ab80878af2f9b69c3ac39e5a97
aosskfdlrla <gmltns885@gmail.com>
2024-08-01 09:54:50 +0900

    chapter 4 실습 정리 커밋

---
+++ chap04-control-flow/src/main/java/com/hitsuni/section01/conditional/Application.java
@@ -0,0 +1,18 @@
+package com.hitsuni.section01.conditional;
+
+public class Application {
+    public static void main(String[] args) {
+        num_if nif = new num_if();
+        // nif.testSimpleIfStatement();
+        // nif.testNestIfStatement();
+
+        num_ifElse numIfElse = new num_ifElse();
+        // numIfElse.testSimpleIfElseStatement();
+        // numIfElse.testNestedIfElseStatement();
+
+        num_IfElseIf numIfElseIf = new num_IfElseIf();
+        //numIfElseIf.testSimpleIfElseIfStatement();
+        //numIfElseIf.testNestIfElseIfStatement();
+        numIfElseIf.importNestIfElseIfStatement();
+    }
+}
