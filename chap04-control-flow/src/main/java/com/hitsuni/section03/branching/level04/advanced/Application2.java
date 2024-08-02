6b0c6a682cfd9b9a018776b0f7f1cf6293778316
aosskfdlrla <gmltns885@gmail.com>
2024-08-02 09:10:09 +0900

    chap04 Java 과제 수행 완료

--- chap04-control-flow/src/main/java/com/hitsuni/section03/branching/level04/advanced/Application2.java
+++ chap04-control-flow/src/main/java/com/hitsuni/section03/branching/level04/advanced/Application2.java
@@ -1,9 +1,45 @@
 package com.hitsuni.section03.branching.level04.advanced;
 
+import java.util.Scanner;
+
 public class Application2 {
     public static void main(String[] args) {
-        while(true) {
-            // 작업중
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("받으신 금액을 입력하세요 : ");
+        int inputMoney = sc.nextInt();
+
+        System.out.print("상품 가격을 입력하세요 : ");
+        int inputPrice = sc.nextInt();
+
+        int change = inputMoney - inputPrice;
+        int currency = 50000;
+        boolean isCurrencySwitch = true;
+
+        System.out.println("===========================");
+
+        while (change > 0) {
+            int count = change / currency;
+
+            if (count > 0) {
+                if (currency >= 1000) {
+                    System.out.println(currency + "원권 지폐 " + count + "장");
+                } else {
+                    System.out.println(currency + "원권 동전 " + count + "개");
+                }
+                change %= currency;
+            }
+
+            if (isCurrencySwitch) {
+                currency /= 5;
+            } else {
+                currency /= 2;
+            }
+
+            isCurrencySwitch = !isCurrencySwitch;
         }
+
+        System.out.println("===========================");
+        System.out.println("거스름돈 : " + (inputMoney - inputPrice) + "원");
     }
 }
