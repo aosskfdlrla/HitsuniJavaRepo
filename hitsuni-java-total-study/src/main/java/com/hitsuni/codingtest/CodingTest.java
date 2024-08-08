7437d8c4f00350d39ca3e148387d436562970e21
aosskfdlrla <gmltns885@gmail.com>
2024-08-08 10:33:04 +0900

    Java BackJoon 문제 10818 푼 코드 커밋

---
+++ hitsuni-java-total-study/src/main/java/com/hitsuni/codingtest/CodingTest.java
@@ -0,0 +1,38 @@
+package com.hitsuni.codingtest;
+
+import java.util.Scanner;
+
+public class CodingTest {
+    public static void main(String[] args) {
+
+        int max = 0, min = 0, index = 0;
+        Scanner scan = new Scanner(System.in);
+        int inputNum = scan.nextInt();
+        int[] numbers = new int[inputNum];
+
+        scan.nextLine();
+
+        String nums = scan.nextLine();
+        String[] numArr = nums.split(" ");
+
+        scan.close();
+
+        for(int i = 0; i < numArr.length; i++) {
+            numbers[i] = Integer.parseInt(numArr[i]);
+        }
+
+        do {
+            if(index != 0) {
+                max = Math.max(max, numbers[index]);
+                min = Math.min(min, numbers[index]);
+                index++;
+            } else {
+                max = numbers[0];
+                min = numbers[0];
+                index++;
+            }
+        }while(index < numbers.length);
+
+        System.out.println(min + " " + max);
+    }
+}
