f864ad36dcf8af893535f086382707f40ab89212
aosskfdlrla <gmltns885@gmail.com>
2024-08-12 17:43:58 +0900

    Java - 컬렉션 실습 1번 완료 커밋

---
+++ chap13-collection-lecture-source/src/main/java/com/hitsuni/section01/level01/basic/Application.java
@@ -0,0 +1,36 @@
+package com.hitsuni.section01.level01.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Application {
+    public static void main(String[] args) {
+        ArrayList<Integer> scoreArr = new ArrayList<Integer>();
+        boolean result = false;
+        int totalScore = 0;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            do {
+                System.out.print("학생 성적 : ");
+                scoreArr.add(Integer.parseInt(br.readLine()));
+                System.out.print("추가 입력하려면 y : ");
+                String str = br.readLine();
+                result = str.equals("Y") || str.equals("y");
+                ;
+            } while(result);
+
+            System.out.println("학생인원 : " + scoreArr.size());
+            for(Integer arr : scoreArr)
+                totalScore += arr;
+
+            System.out.println("평균점수 : " + (float)(totalScore / scoreArr.size()));
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("프로그램이 종료되었습니다.");
+        }
+    }
+}
+
