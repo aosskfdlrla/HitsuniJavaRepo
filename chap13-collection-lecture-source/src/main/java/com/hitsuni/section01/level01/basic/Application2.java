fbca832881a3f09d881674fc717303e28ee1ee39
aosskfdlrla <gmltns885@gmail.com>
2024-08-13 17:27:06 +0900

    Java - 컬렉션 과제 2번 완료 커밋

--- chap13-collection-lecture-source/src/main/java/com/hitsuni/section01/level01/basic/Application2.java
+++ chap13-collection-lecture-source/src/main/java/com/hitsuni/section01/level01/basic/Application2.java
@@ -1,10 +1,30 @@
 package com.hitsuni.section01.level01.basic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Application2 {
     public static void main(String[] args) {
         Stack<String> visitWebSite = new Stack<String>();
+        boolean exit = true;
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            do {
+                System.out.print("방문 URL을 입력하세요 (단, exit를 입력하면 종료) : ");
+                String inputWebSite = br.readLine();
+                if(!inputWebSite.equals("exit")) {
+                    visitWebSite.push(inputWebSite);
+                    System.out.println(visitWebSite);
+                } else {
+                    exit = false;
+                }
+            } while(exit);
 
+            for(int i=0; i < 5; i++)
+                System.out.println(visitWebSite.get(i));
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
